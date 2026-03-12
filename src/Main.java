158ff84f868bb5cf238ded5cc33414c7063be378
sammy-ryed <edwardsamarth.com>
2026-03-12 09:08:13 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Hotel Booking Management System
+ * @author YourName
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * Main entry point of the application.
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay!");
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: 1.0");
+    }
+}
